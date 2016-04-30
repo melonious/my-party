57dd98e0fdb92bcde6b120e134d2acafe4cf01a2
Deri Cleland <dericleland@me.com>
2016-04-30 16:20:06 -0400

    restaurant list

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
+Restaurant.create([{name:'Seito',description:'Asian Fusion'}, {name:'Maggianos', description:'Traditional Italian Cuisine' }, {name: 'Rise&Dine',description: 'Breakfast served all day'}, {name: 'Margaritas', description: 'Mexican Cantina'}, {name:'Big Fish Seafood', description: 'Fine dining seafood'}])
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
